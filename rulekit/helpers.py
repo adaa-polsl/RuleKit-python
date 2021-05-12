fix: mapping numerical label values
+remove unnecessary file

rulekit/helpers.py
@@ -255,24 +255,27 @@ class PredictionResultMapper:
         prediction = []
         row_reader = predicted_example_set.getExampleTable().getDataRowReader()
         attribute = predicted_example_set.getAttributes().get('prediction')
+        label_mapping = attribute.getMapping()
         while row_reader.hasNext():
             row = row_reader.next()
             value_index = row.get(attribute)
-            value = attribute.getMapping().mapIndex(round(value_index))
+            value = label_mapping.mapIndex(round(value_index))
             prediction.append(value)
         prediction = list(map(str, prediction))
-        return np.array(prediction)
+        return np.array(prediction).astype(np.unicode_)
 
     @staticmethod
-    def map_to_numerical(predicted_example_set) -> np.ndarray:
+    def map_to_numerical(predicted_example_set, remap: bool=True) -> np.ndarray:
         prediction = []
         row_reader = predicted_example_set.getExampleTable().getDataRowReader()
         attribute = predicted_example_set.getAttributes().get('prediction')
+        label_mapping = predicted_example_set.getAttributes().getLabel().getMapping()
         while row_reader.hasNext():
             row = row_reader.next()
-            value = attribute.getValue(row)
+            value = int(attribute.getValue(row))
+            if remap:
+                value = float(str(label_mapping.mapIndex(value)))
             prediction.append(value)
-        prediction = list(map(float, prediction))
         return np.array(prediction)
 
     @staticmethod

rulekit/regression.py
@@ -3,7 +3,7 @@ from numbers import Number
 import numpy as np
 import pandas as pd
 from sklearn import metrics
-
+from .helpers import PredictionResultMapper
 from .operator import BaseOperator, ExpertKnowledgeOperator, Data
 from .params import Measures
 
@@ -104,6 +104,9 @@ class RuleRegressor(BaseOperator):
         """
         predicted_labels = self.predict(values)
         return metrics.r2_score(labels, predicted_labels)
+
+    def _map_result(self, predicted_example_set) -> np.ndarray:
+        return PredictionResultMapper.map_to_numerical(predicted_example_set, remap=False)
 
 
 class ExpertRuleRegressor(ExpertKnowledgeOperator, RuleRegressor):