8ae129246ca0cecb94ab1a47af9ac108d0a320e8
cezary.maszczyk <cezary.maszczyk@emag.lukasiewicz.gov.pl>
2024-02-05 10:26:57 +0100

    fix: Issue#17 update to RuleKit java package v1.7.3

--- rulekit/__init__.py
+++ rulekit/__init__.py
@@ -1,5 +1,5 @@
 # pylint: disable=missing-module-docstring
 from .main import RuleKit
 
-__VERSION__ = '1.7.1'
-__RULEKIT_RELEASE_VERSION__ = '1.7.1'
+__VERSION__ = '1.7.4'
+__RULEKIT_RELEASE_VERSION__ = '1.7.3'
