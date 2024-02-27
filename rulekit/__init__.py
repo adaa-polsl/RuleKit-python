90780cde43df0dd39b2a0138d3b12dc731a01dc3
cezary.maszczyk <cezary.maszczyk@emag.lukasiewicz.gov.pl>
2024-02-27 10:07:23 +0100

    bump package version to 1.7.6

--- rulekit/__init__.py
+++ rulekit/__init__.py
@@ -1,5 +1,5 @@
 # pylint: disable=missing-module-docstring
 from .main import RuleKit
 
-__VERSION__ = '1.7.5'
-__RULEKIT_RELEASE_VERSION__ = '1.7.4'
+__VERSION__ = '1.7.6'
+__RULEKIT_RELEASE_VERSION__ = '1.7.5'
