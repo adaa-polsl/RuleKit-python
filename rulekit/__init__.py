7333df9fa29111ca42b8cc0677e653cb005def92
cezary.maszczyk <cezary.maszczyk@emag.lukasiewicz.gov.pl>
2024-07-29 12:32:15 +0200

    update versions to v2.1.15.0

--- rulekit/__init__.py
+++ rulekit/__init__.py
@@ -1,5 +1,5 @@
 # pylint: disable=missing-module-docstring
 from .main import RuleKit
 
-__VERSION__ = '1.7.14.0'
-__RULEKIT_RELEASE_VERSION__ = '1.7.14'
+__VERSION__ = '2.1.15.0'
+__RULEKIT_RELEASE_VERSION__ = '2.1.15'
