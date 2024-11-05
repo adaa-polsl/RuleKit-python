af12efb83e3ad26f62d0518393658389d9bb04e5
cezary.maszczyk <cezary.maszczyk@emag.lukasiewicz.gov.pl>
2024-11-05 12:23:28 +0100

    build: Update documentation build script

--- docs/build.py
+++ docs/build.py
@@ -1,10 +1,17 @@
 import argparse
 import os
+import pathlib
 import shutil
 import subprocess
 import sys
+from typing import Optional
 
-curr_dir_path: str = os.path.dirname(os.path.realpath(__file__))
+curr_dir_path: str = pathlib.Path(os.path.realpath(__file__)).parent
+
+
+def run_command(command: str, cwd: Optional[str] = None):
+    output = subprocess.check_output(command, cwd=cwd)
+    print(output.decode())
 
 
 def build_docs_using_sphinx(version_number: str):
@@ -15,19 +22,18 @@ def build_docs_using_sphinx(version_number: str):
 
     python_path: str = sys.executable
     output_path: str = f'{curr_dir_path}/serve/v{version_number}'
-    output = subprocess.check_output(
+    run_command(
         f"{python_path} -m sphinx.cmd.build -M html source {output_path}"
     )
-    print(output.decode())
-
     tmp_path: str = f'{output_path}@'
     shutil.move(output_path, tmp_path)
-    shutil.move(os.path.join(tmp_path, 'html'), output_path)
+    shutil.move(pathlib.Path(tmp_path, 'html'), output_path)
     shutil.rmtree(tmp_path)
 
 
 def update_index_html(version_number: str):
-    with open(os.path.join(curr_dir_path, 'serve', 'index.html'), 'r', encoding='utf-8') as index_html:
+    print('Updating index.html...')
+    with open(pathlib.Path(curr_dir_path / 'serve' / 'index.html'), 'r', encoding='utf-8') as index_html:
         content: str = index_html.read()
         content = content.replace('(latest)', '')
         content = content.replace(
@@ -41,8 +47,59 @@ def update_index_html(version_number: str):
                 <!-- LATEST VERSION PLACEHOLDER -->
             '''
         )
-    with open(os.path.join(curr_dir_path, 'serve', 'index.html'), 'w', encoding='utf-8') as index_html:
+    with open(pathlib.Path(curr_dir_path / 'serve' / 'index.html'), 'w', encoding='utf-8') as index_html:
         index_html.write(content)
+
+
+def update_badges():
+    print('Updating badges')
+    cwd = pathlib.Path(curr_dir_path / '..')  # root repo dir
+
+    # update tests coverage badge
+    python_path: str = sys.executable
+    run_command(
+        f"{python_path} -m coverage run -m unittest discover ./tests",
+        cwd=cwd
+    )
+    run_command(
+        f"{python_path} -m coverage xml -o ./docs/reports/coverage/coverage.xml",
+        cwd=cwd
+    )
+    run_command(
+        f"{python_path} -m coverage html -d ./docs/reports/coverage/",
+        cwd=cwd
+    )
+    run_command(
+        "genbadge coverage -i ./docs/reports/coverage/coverage.xml  -o ./docs/badges/coverage-badge.svg",
+        cwd=cwd
+    )
+
+    # update test badge
+    test_report_path = pathlib.Path(curr_dir_path / 'reports' / 'junit')
+    shutil.rmtree(test_report_path)
+    os.makedirs(test_report_path, exist_ok=True)
+    run_command(
+        f"{python_path} -m junitxml.main --o ./docs/reports/junit/junit.xml",
+        cwd=cwd
+    )
+    run_command(
+        f"{python_path} -m junit2htmlreport ./docs/reports/junit/junit.xml ./docs/reports/junit/report.html",
+        cwd=cwd
+    )
+    run_command(
+        "genbadge tests -i ./docs/reports/junit/junit.xml -o ./docs/badges/test-badge.svg",
+        cwd=cwd
+    )
+
+    # update flake8 badge
+    run_command(
+        f"{python_path} -m flake8 ./rulekit --exit-zero --format=html --htmldir ./docs/reports/flake8 --statistics --tee --output-file ./docs/reports/flake8/flake8stats.txt",
+        cwd=cwd
+    )
+    run_command(
+        "genbadge flake8 -i ./docs/reports/flake8/flake8stats.txt -o ./docs/badges/flake8-badge.svg",
+        cwd=cwd
+    )
 
 
 def main():
@@ -54,6 +111,8 @@ def main():
 
     build_docs_using_sphinx(args.version_number)
     update_index_html(args.version_number)
+    update_badges()
+    print('Done!')
 
 
 if __name__ == "__main__":
