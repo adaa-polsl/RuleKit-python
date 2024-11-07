055e5e4c499de812d474464418323974531681f0
cezary.maszczyk <cezary.maszczyk@emag.lukasiewicz.gov.pl>
2024-11-07 12:56:47 +0100

    feat: Add types for used defined measures

--- rulekit/_helpers.py
+++ rulekit/_helpers.py
@@ -3,6 +3,7 @@
 import io
 import json
 from typing import Any
+from typing import Callable
 from typing import Optional
 from typing import Union
 
@@ -44,6 +45,10 @@ def get_rule_generator(expert: bool = False) -> Any:
 class RuleGeneratorConfigurator:
     """Class for configuring rule induction parameters
     """
+
+    _MEASURES_PARAMETERS: list[str] = [
+        'induction_measure', 'pruning_measure', 'voting_measure',
+    ]
 
     def __init__(self, rule_generator):
         self.rule_generator = rule_generator
@@ -96,16 +101,21 @@ class RuleGeneratorConfigurator:
             if isinstance(param_value, Measures):
                 self.rule_generator.setParameter(
                     param_name, param_value.value)
-            if isinstance(param_value, str):
+            if isinstance(param_value, Callable):
+                self._configure_user_defined_measure_parameter(
+                    param_name, param_value)
                 self.rule_generator.setParameter(param_name, 'UserDefined')
                 self.rule_generator.setParameter(param_name, param_value)
 
+    def _configure_user_defined_measure_parameter(self, param_name: str, param_value: Any):
+        if param_value is not None:
+            self.rule_generator.setParameter(param_name, 'UserDefined')
+            self.rule_generator.setParameter(param_name, param_value)
+
     def _configure_rule_generator(self, **kwargs: dict[str, Any]):
-        if kwargs.get('induction_measure') == Measures.LogRank or \
-                kwargs.get('pruning_measure') == Measures.LogRank or \
-                kwargs.get('voting_measure') == Measures.LogRank:
+        if any([kwargs.get(param_name) == Measures.LogRank for param_name in self._MEASURES_PARAMETERS]):
             self.LogRank = JClass('adaa.analytics.rules.logic.quality.LogRank')
-        for measure_param_name in ['induction_measure', 'pruning_measure', 'voting_measure']:
+        for measure_param_name in self._MEASURES_PARAMETERS:
             measure_param_value: Measures = kwargs.pop(
                 measure_param_name, None)
             self._configure_measure_parameter(
