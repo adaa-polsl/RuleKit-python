e099cb234e600c4ebd2edd8305423a2e70758b7a
cezary.maszczyk <cezary.maszczyk@emag.lukasiewicz.gov.pl>
2024-11-07 10:20:37 +0100

    build: Github action to automate documentation generation after new release

--- docs/generate_readme_badges.py
+++ docs/generate_readme_badges.py
@@ -9,50 +9,59 @@ curr_dir_path: str = pathlib.Path(os.path.realpath(__file__)).parent
 
 
 # Documentation will be automatically generated on new version release
-def generate_tests_coverage_badge(python_path: str, cwd: str):
-    run_command(
-        f"{python_path} -m coverage run -m unittest discover ./tests",
+def generate_tests_coverage_badge(cwd: str):
+    run_command([
+        sys.executable, "-m", "coverage", "run", "-m", "unittest",  "discover", "./tests"
+    ],
         cwd=cwd
     )
-    run_command(
-        f"{python_path} -m coverage xml -o ./docs/reports/coverage/coverage.xml",
+    run_command([
+        sys.executable, "-m", "coverage", "xml", "-o", "./docs/reports/coverage/coverage.xml"
+    ],
         cwd=cwd
     )
-    run_command(
-        f"{python_path} -m coverage html -d ./docs/reports/coverage/",
+    run_command([
+        sys.executable, "-m", "coverage", "html", "-d", "./docs/reports/coverage/"
+    ],
         cwd=cwd
     )
-    run_command(
-        "genbadge coverage -i ./docs/reports/coverage/coverage.xml  -o ./docs/badges/coverage-badge.svg",
+    run_command([
+        "genbadge", "coverage", "-i", "./docs/reports/coverage/coverage.xml", "-o", "./docs/badges/coverage-badge.svg"
+    ],
         cwd=cwd
     )
 
 
-def generate_test_badge(python_path: str, cwd: str):
+def generate_test_badge(cwd: str):
     test_report_path = pathlib.Path(curr_dir_path / 'reports' / 'junit')
     shutil.rmtree(test_report_path)
     os.makedirs(test_report_path, exist_ok=True)
-    run_command(
-        f"{python_path} -m junitxml.main --o ./docs/reports/junit/junit.xml",
+    run_command([
+        sys.executable, "-m", "junitxml.main", "--o", "./docs/reports/junit/junit.xml"
+    ],
         cwd=cwd
     )
-    run_command(
-        f"{python_path} -m junit2htmlreport ./docs/reports/junit/junit.xml ./docs/reports/junit/report.html",
+    run_command([
+        sys.executable, "-m", "junit2htmlreport", "./docs/reports/junit/junit.xml", "./docs/reports/junit/report.html"
+    ],
         cwd=cwd
     )
-    run_command(
-        "genbadge tests -i ./docs/reports/junit/junit.xml -o ./docs/badges/test-badge.svg",
+    run_command([
+        "genbadge", "tests", "-i", "./docs/reports/junit/junit.xml", "-o", "./docs/badges/test-badge.svg"
+    ],
         cwd=cwd
     )
 
 
-def generate_flake8_badge(python_path: str, cwd: str):
-    run_command(
-        f"{python_path} -m flake8 ./rulekit --exit-zero --format=html --htmldir ./docs/reports/flake8 --statistics --tee --output-file ./docs/reports/flake8/flake8stats.txt",
+def generate_flake8_badge(cwd: str):
+    run_command([
+        sys.executable, "-m", "flake8", "./rulekit", "--exit-zero", "--format=html", "--htmldir", "./docs/reports/flake8", "--statistics", "--tee", "--output-file", "./docs/reports/flake8/flake8stats.txt"
+    ],
         cwd=cwd
     )
-    run_command(
-        "genbadge flake8 -i ./docs/reports/flake8/flake8stats.txt -o ./docs/badges/flake8-badge.svg",
+    run_command([
+        "genbadge", "flake8", "-i", "./docs/reports/flake8/flake8stats.txt", "-o", "./docs/badges/flake8-badge.svg"
+    ],
         cwd=cwd
     )
 
@@ -60,11 +69,10 @@ def generate_flake8_badge(python_path: str, cwd: str):
 def main():
     print('Updating badges')
     cwd = pathlib.Path(curr_dir_path / '..')  # root repo dir
-    python_path: str = sys.executable
 
-    generate_tests_coverage_badge(python_path, cwd)
-    generate_test_badge(python_path, cwd)
-    generate_flake8_badge(python_path, cwd)
+    generate_tests_coverage_badge(cwd)
+    generate_test_badge(cwd)
+    generate_flake8_badge(cwd)
 
     print('Badges generated successfully!')
 
--- docs/helpers.py
+++ docs/helpers.py
@@ -2,6 +2,17 @@ import subprocess
 from typing import Optional
 
 
-def run_command(command: str, cwd: Optional[str] = None):
-    output = subprocess.check_output(command, cwd=cwd)
+def run_command(command: list[str], cwd: Optional[str] = None, raise_on_error: bool = True):
+    process = subprocess.Popen(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        cwd=cwd
+    )
+    output, error = process.communicate()
+    if process.returncode != 0 and raise_on_error:
+        raise Exception("File handling failed %d %s %s" %
+                        (process.returncode, output, error)
+                        )
+
     print(output.decode())
